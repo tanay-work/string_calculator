3a1b593c9f6e29d44356be645a36eace7446bf03
Tanay <tanaywork21@gmail.com>
2024-10-16 20:55:52 +0530

    add tests for big numbers (more than 1000)

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -37,4 +37,12 @@ class StringCalculatorTest < Minitest::Test
   def test_add_with_multiple_negative_numbers
     assert_raises(RuntimeError, 'negative numbers not allowed -1,-2') { @calculator.add('-1,-2') }
   end
+
+  def test_add_ignores_numbers_bigger_than_1000
+    assert_equal 2, @calculator.add('2,1001')
+  end
+
+  def test_add_with_custom_delimiter_and_bigger_numbers
+    assert_equal 20, @calculator.add("//;\n1;2;3;4;\n\n\n;0\n\n\n;5;0;1002;9874;4444;5")
+  end
 end
