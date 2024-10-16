b1775dece49c5954404829a6bfa374dd07c36fc9
Tanay <tanaywork21@gmail.com>
2024-10-16 17:51:21 +0530

    rubocop fixes

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -1,21 +1,24 @@
+# frozen_string_literal: true
+
 # string_calculator_test.rb
 require 'minitest/autorun'
 require_relative 'string_calculator'
 
+# Test cases for calculator class
 class StringCalculatorTest < Minitest::Test
   def setup
     @calculator = StringCalculator.new
   end
 
   def test_add_empty_string
-    assert_equal 0, @calculator.add("")
+    assert_equal 0, @calculator.add('')
   end
 
   def test_add_single_number
-    assert_equal 1, @calculator.add("1")
+    assert_equal 1, @calculator.add('1')
   end
 
   def test_add_two_numbers
-    assert_equal 3, @calculator.add("1,2")
+    assert_equal 3, @calculator.add('1,2')
   end
 end
