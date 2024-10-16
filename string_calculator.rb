f822775a89cb303ddaf2ba434d70a3694c6cac3e
Tanay <tanaywork21@gmail.com>
2024-10-16 20:25:42 +0530

    optimize add method for default and custom delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,14 +5,15 @@ class StringCalculator
   def add(input)
     return 0 if input.empty?
 
+    delimiter = ',' # set default delimiter
+
     # delimiter condition
-    if input.start_with?("//")
-      delimiter = input[2]
-      input = input[4..-1]
-      input = input.gsub(delimiter, ",")
+    if input.start_with?('//')
+      delimiter = input.match(%r{//(.)\n})[1] # extract custom delimiter from the input
     end
 
-    input = input.gsub("\n", ",")
-    input.split(',').map(&:to_i).sum
+    input = input.gsub(%r{//[^\n]+\n}, '')
+    input.gsub!("\n", delimiter)
+    input.split(delimiter.to_s).map(&:to_i).sum
   end
 end
